f5b6b33670946502c89a3ca8f8e7e902de6a7ba9
Anik Das <anikdas0811@gmail.com>
2020-05-19 17:47:05 +0530

    Added more understandable comments

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
-# Init
+# Init flask app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# database
+# database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init db
 db = SQLAlchemy(app)
-# init marshmallow
+# init marshmallow (object deserialisation)
 ma = Marshmallow(app)
+# marshmallow is used to convert datatypes into python readable data
 
 # product class/model
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    # id being primary integer means its value will increase automatically on product addition
     name = db.Column(db.String(100),unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
-
+    # creating constructor to assign passed values to created object
     def __init__(self, name, description, price, qty):
         self.name = name
         self.description = description
@@ -37,9 +39,10 @@ class ProductSchema(ma.Schema):
 
 # init schema
 product_schema = ProductSchema()
+# for multiple prodcuts
 products_schema = ProductSchema(many=True)
 
-# create a product
+# route to create a product
 
 @app.route('/product', methods=['POST'])
 def add_product():
@@ -54,40 +57,40 @@ def add_product():
 
     return product_schema.jsonify(new_product)
 
-# Get all products
+# route to Get all products
 @app.route("/product", methods=['GET'])
 def get_products():
     all_products = Product.query.all()
     result = products_schema.dump(all_products)
     return jsonify(result)
 
-# Get single products
+# route to Get single products
 @app.route("/product/<id>", methods=['GET'])
 def get_product(id):
     product = Product.query.get(id)
     return product_schema.jsonify(product)
 
-# update a product
+# route to update a product
 @app.route('/product/<id>', methods=['PUT'])
 def update_product(id):
     product = Product.query.get(id)
-
+    # get data from response
     name = request.json['name']
     description = request.json['description']
     price = request.json['price']
     qty = request.json['qty']
-
+    # overwrite db data with response data
     product.name = name
     product.description = description
     product.price - price
     product.qty = qty
-
+    # commit necessary to update data
     db.session.commit()
 
     return product_schema.jsonify(product)
 
 
-# deleted single product
+# route to deleted a single product
 @app.route("/product/<id>", methods=['DELETE'])
 def delete_product(id):
     product = Product.query.get(id)
